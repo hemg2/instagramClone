51c2036e5a1912242f67c0bf681376a1222fcddb
hemg2 <hemg2@naver.com>
2022-07-11 22:27:19 +0900

    7.11 main스토리

---
+++ instagramClone/Home/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  instagramClone
+//
+//  Created by 1 on 2022/07/11.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "InstagramMainTableViewCell", for: indexPath)
+
+    }
+    
+    
+}
---
+++ instagramClone/Home/View/InstagramMainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InstagramMainTableViewCell.swift
+//  instagramClone
+//
+//  Created by 1 on 2022/07/11.
+//
+
+import UIKit
+
+class InstagramMainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
